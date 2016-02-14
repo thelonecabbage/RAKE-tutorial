411b59ca262b8bf4a85c3b9144e18f3e1fc5177c
bluevine-dev/thelonecabbage <thelonecabbage@gmail.com>
2016-02-15 00:00:45 +0200

    OOPed, stemming, optional word extraction

--- rake.py
+++ rake.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 # Implementation of RAKE - Rapid Automtic Keyword Exraction algorithm
 # as described in:
 # Rose, S., D. Engel, N. Cramer, and W. Cowley (2010).
@@ -10,13 +12,13 @@
 # as well as the ability to set frequency and phrase length parameters
 # important when dealing with longer documents
 
-import re
+import itertools
 import operator
+import re
+import string
+
 import nltk
 import nltk.data
-import string
-import itertools
-
 
 debug = False
 test = False
@@ -36,7 +38,7 @@ def load_stop_words(stop_word_file):
     return set(stop_words)
 
 sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-stop_words = load_stop_words('static/stopwords.lst')
+
 # stop_words = set(nltk.corpus.stopwords.words('english'))
 # with open("static/stopwords.lst", "r") as word_list:
 #     self.stop_words = self.stop_words.union([w.lower().strip() for w in word_list])
@@ -62,43 +64,9 @@ def is_number(s):
         return False
 
 
-def extract_candidate_chunks(text, max_words_length=5, grammar=r'KT: {(<JJ>* <NN.*>+ <IN>)? <JJ>* <NN.*>+}'):
-    # exclude candidates that are stop words or entirely punctuation
-    global stop_words
-    punct = set(string.punctuation)
-
-    # tokenize, POS-tag, and chunk using regular expressions
-    chunker = nltk.chunk.regexp.RegexpParser(grammar)
-    tagged_sents = nltk.pos_tag_sents(nltk.word_tokenize(
-        sent) for sent in nltk.sent_tokenize(text))
-    all_chunks = list(itertools.chain.from_iterable(nltk.chunk.tree2conlltags(chunker.parse(tagged_sent))
-                                                    for tagged_sent in tagged_sents))
-    # join constituent chunk words into a single chunked phrase
-    candidates = [' '.join(word for word, pos, chunk in group).lower()
-                  for key, group in itertools.groupby(all_chunks, lambda (word, pos, chunk): chunk != 'O') if key]
-
-    return [cand for cand in candidates
-            if cand not in stop_words
-            and not all(char in punct for char in cand)
-            and not len(nltk.word_tokenize(cand)) > max_words_length]
-
 # extract_candidate_chunks = Memoize(extract_candidate_chunks)
 
 
-def extract_candidate_words(text, good_tags=set(['JJ', 'JJR', 'JJS', 'NN', 'NNP', 'NNS', 'NNPS'])):
-    # exclude candidates that are stop words or entirely punctuation
-    global stop_words
-    punct = set(string.punctuation)
-    # stop_words = set(nltk.corpus.stopwords.words('english'))
-    # tokenize and POS-tag words
-    tagged_words = itertools.chain.from_iterable(nltk.pos_tag_sents(nltk.word_tokenize(sent)
-                                                                    for sent in nltk.sent_tokenize(text)))
-    # filter on certain POS tags and lowercase all words
-    candidates = [word.lower() for word, tag in tagged_words
-                  if tag in good_tags and word.lower() not in stop_words
-                  and not all(char in punct for char in word)]
-
-    return candidates
 # extract_candidate_words = Memoize(extract_candidate_words)
 
 
@@ -144,102 +112,112 @@ def build_stop_word_regex(stop_word_file_path):
     return stop_word_pattern
 
 
-def generate_candidate_keywords(sentence_list, stopword_pattern, min_char_length=1, max_words_length=5):
-    phrase_list = []
-    for s in sentence_list:
-        tmp = re.sub(stopword_pattern, '|', s.strip())
-        phrases = tmp.split("|")
-        for phrase in phrases:
-            phrase = phrase.strip().lower()
-            if phrase != "" and is_acceptable(phrase, min_char_length, max_words_length):
-                phrase_list.append(phrase)
-    return phrase_list
-
-
-def is_acceptable(phrase, min_char_length, max_words_length):
-
-    # a phrase must have a min length in characters
-    if len(phrase) < min_char_length:
-        return 0
-
-    # a phrase must have a max number of words
-    words = phrase.split()
-    if len(words) > max_words_length:
-        return 0
-
-    digits = 0
-    alpha = 0
-    for i in range(0, len(phrase)):
-        if phrase[i].isdigit():
-            digits += 1
-        elif phrase[i].isalpha():
-            alpha += 1
-
-    # a phrase must have at least one alpha character
-    if alpha == 0:
-        return 0
-
-    # a phrase must have more alpha than digits characters
-    if digits > alpha:
-        return 0
-    return 1
-
-
-def calculate_word_scores(phraseList, min_char_length=0):
-    word_frequency = {}
-    word_degree = {}
-    global stop_words
-    for phrase in phraseList:
-        word_list = separate_words(phrase, min_char_length)
-        word_list_length = len(word_list)
-        word_list_degree = word_list_length - 1
-        # if word_list_degree > 3: word_list_degree = 3 #exp.
-        for word in word_list:
-            word_frequency.setdefault(word, 0)
-            word_frequency[word] += 1
-            word_degree.setdefault(word, 0)
-            word_degree[word] += word_list_degree  # orig.
-            # word_degree[word] += 1/(word_list_length*1.0) #exp.
-    for item in word_frequency:
-        word_degree[item] = word_degree[item] + word_frequency[item]
-
-    # Calculate Word scores = deg(w)/frew(w)
-    word_score = {}
-    for item in word_frequency:
-        word_score.setdefault(item, 0)
-        # orig.
-        if item.lower() in stop_words:
-            word_score[item] = 0
-        else:
-            word_score[item] = word_degree[item] / (word_frequency[item] * 1.0)
-    # word_score[item] = word_frequency[item]/(word_degree[item] * 1.0) #exp.
-    return word_score
-
-
-def generate_candidate_keyword_scores(phrase_list, word_score, min_keyword_frequency=1, min_char_length=0):
-    keyword_candidates = {}
-
-    for phrase in phrase_list:
-        if min_keyword_frequency > 1:
-            if phrase_list.count(phrase) < min_keyword_frequency:
-                continue
-        keyword_candidates.setdefault(phrase, 0)
-        word_list = separate_words(phrase, min_char_length)
-        candidate_score = 0
-        for word in word_list:
-            candidate_score += word_score[word]
-        keyword_candidates[phrase] = candidate_score
-    return keyword_candidates
+def noop(txt):
+    return txt
 
 
 class Rake(object):
 
-    def __init__(self, stop_words_path, min_char_length=1, max_words_length=5, min_keyword_frequency=1):
+    def __init__(self, stop_words_path, min_char_length=1, max_words_length=5, min_keyword_frequency=1,
+                 extractor='chunks', stemming=False):
         self.__stop_words_path = stop_words_path
         self.__stop_words_pattern = build_stop_word_regex(stop_words_path)
+        self.__stop_words = load_stop_words(stop_words_path)
         self.__min_char_length = min_char_length
         self.__max_words_length = max_words_length
         self.__min_keyword_frequency = min_keyword_frequency
+        self.__stem = nltk.stem.lancaster.LancasterStemmer().stem if stemming else noop
+        self.__end = re.compile('\W+$', re.UNICODE)
+        self.__begin = re.compile('^\W+', re.UNICODE)
+        self.__invalid_char = re.compile(u"[^\w\d\-''’`]+",  re.UNICODE)
+        self.__extractor = self.__extract_candidate_chunks if extractor == 'chunks' else self.__extract_candidate_words
+
+    def __trim(self, txt):
+        txt = self.__invalid_char.sub(' ', txt)
+        txt = self.__begin.sub('', txt)
+        txt = self.__end.sub('', txt)
+        txt = txt.replace('  ', ' ')
+        return txt.lower()
+
+    def __generate_candidate_keyword_scores(self, phrase_list, word_score):
+        keyword_candidates = {}
+
+        for phrase in phrase_list:
+            if self.__min_keyword_frequency > 1:
+                if phrase_list.count(phrase) < self.__min_keyword_frequency:
+                    continue
+            keyword_candidates.setdefault(phrase, 0)
+            word_list = separate_words(phrase, self.__min_char_length)
+            candidate_score = 0
+            for word in word_list:
+                try:
+                    candidate_score += word_score[word]
+                except KeyError:
+                    pass
+            keyword_candidates[phrase] = candidate_score
+        return keyword_candidates
+
+    def __calculate_word_scores(self, phraseList):
+        word_frequency = {}
+        word_degree = {}
+        for phrase in phraseList:
+            word_list = separate_words(phrase, self.__min_char_length)
+            word_list_length = len(word_list)
+            word_list_degree = word_list_length - 1
+            # if word_list_degree > 3: word_list_degree = 3 #exp.
+            for word in word_list:
+                word_frequency.setdefault(word, 0)
+                word_frequency[word] += 1
+                word_degree.setdefault(word, 0)
+                word_degree[word] += word_list_degree  # orig.
+                # word_degree[word] += 1/(word_list_length*1.0) #exp.
+        for item in word_frequency:
+            word_degree[item] = word_degree[item] + word_frequency[item]
+
+        # Calculate Word scores = deg(w)/frew(w)
+        word_score = {}
+        for item in word_frequency:
+            word_score.setdefault(item, 0)
+            # orig.
+            if item.lower() in self.__stop_words:
+                word_score[item] = 0
+            else:
+                word_score[item] = word_degree[item] / (word_frequency[item] * 1.0)
+        # word_score[item] = word_frequency[item]/(word_degree[item] * 1.0) #exp.
+        return word_score
+
+    def __extract_candidate_chunks(self, text, grammar=r'KT: {(<JJ>* <NN.*>+ <IN>)? <JJ>* <NN.*>+}'):
+        # exclude candidates that are stop words or entirely punctuation
+        punct = set(string.punctuation)
+
+        # tokenize, POS-tag, and chunk using regular expressions
+        chunker = nltk.chunk.regexp.RegexpParser(grammar)
+        tagged_sents = nltk.pos_tag_sents(nltk.word_tokenize(
+            sent) for sent in nltk.sent_tokenize(text))
+        all_chunks = list(itertools.chain.from_iterable(nltk.chunk.tree2conlltags(chunker.parse(tagged_sent))
+                                                        for tagged_sent in tagged_sents))
+        # join constituent chunk words into a single chunked phrase
+        candidates = [' '.join(word for word, pos, chunk in group).lower()
+                      for key, group in itertools.groupby(all_chunks, lambda (word, pos, chunk): chunk != 'O') if key]
+
+        return [self.__trim(cand) for cand in candidates
+                if cand not in self.__stop_words
+                and not all(char in punct for char in cand)
+                and not len(nltk.word_tokenize(cand)) > self.__max_words_length]
+
+    def __extract_candidate_words(self, text, good_tags=set(['JJ', 'JJR', 'JJS', 'NN', 'NNP', 'NNS', 'NNPS'])):
+        # exclude candidates that are stop words or entirely punctuation
+        punct = set(string.punctuation)
+        # stop_words = set(nltk.corpus.stopwords.words('english'))
+        # tokenize and POS-tag words
+        tagged_words = itertools.chain.from_iterable(nltk.pos_tag_sents(nltk.word_tokenize(sent)
+                                                                        for sent in nltk.sent_tokenize(text)))
+        # filter on certain POS tags and lowercase all words
+        candidates = [self.__trim(word) for word, tag in tagged_words
+                      if tag in good_tags and word.lower() not in self.__stop_words
+                      and not all(char in punct for char in word)]
+
+        return candidates
 
     def run(self, text):
         sentence_list = split_sentences(text)
@@ -248,14 +226,12 @@ class Rake(object):
         # sentence_list, self.__stop_words_pattern, self.__min_char_length,
         # self.__max_words_length)
 
-        phrase_list = extract_candidate_chunks(text, self.__max_words_length)
+        phrase_list = self.__extractor(text)
 
-        word_scores = calculate_word_scores(
-            phrase_list, self.__min_char_length)
+        word_scores = self.__calculate_word_scores(sentence_list)
         # word_scores = calculate_word_scores(sentence_list, self.__min_char_length)
 
-        keyword_candidates = generate_candidate_keyword_scores(
-            phrase_list, word_scores, self.__min_keyword_frequency, self.__min_char_length)
+        keyword_candidates = self.__generate_candidate_keyword_scores(phrase_list, word_scores)
 
         # import ipdb; ipdb.set_trace()
 
@@ -263,25 +239,33 @@ class Rake(object):
         ), key=operator.itemgetter(1), reverse=True)
         return sorted_keywords
 
-    def summaries(self, text, keywords=[]):
-        sentence_list = [[sent, 0] for sent in split_sentences(text)]
-        keywords = [k.lower() for k in keywords]
-        kw_scores = {}
-        for kw in self.run(text):
-            if not keywords or kw[0] in keywords:
-                kw_scores[kw[0]] = (kw[0], kw[1] or 1)
-        for kw in list(set(keywords) - set(kw_scores.keys())):
-            kw_scores[kw] = (kw, 1)
+    def summaries(self, text, phrase_list=[], max_word_length=40):
+        sentence_list = [s.lower() for s in split_sentences(text)]
+        if not phrase_list:
+            phrase_list = self.__extractor(text)
+        phrase_list = [k.lower() for k in phrase_list]
 
-        print kw_scores.values()
+        word_scores = self.__calculate_word_scores(sentence_list)
+        keyword_candidates = self.__generate_candidate_keyword_scores(phrase_list, word_scores)
 
+        scored_sentence_list = []
         for sent in sentence_list:
-            for kw, score in kw_scores.values():
-                sent[1] += score if sent[0].find(kw) >= 0 else 0
-            sent[1] /= len(nltk.word_tokenize(sent[0]))
+            score = 0
+            went_words = nltk.word_tokenize(sent)
+            if len(went_words) > max_word_length:
+                continue
+            stemmed_sent = ' '.join([self.__stem(w) for w in went_words])
+            kw_list = []
+            for kw in keyword_candidates:
+                # cnt = sent.count(kw)
+                cnt = stemmed_sent.count(self.__stem(kw))
+                if cnt:
+                    score += keyword_candidates[kw] * cnt
+                    kw_list.append((kw, keyword_candidates[kw]))
+            score /= (len(went_words) / (len(kw_list) or 1))
+            scored_sentence_list.append((sent, score))
 
-        sentence_list.sort(key=operator.itemgetter(1), reverse=True)
-        return sentence_list
+        return sorted(scored_sentence_list, key=operator.itemgetter(1), reverse=True)
 
 
 if test:
